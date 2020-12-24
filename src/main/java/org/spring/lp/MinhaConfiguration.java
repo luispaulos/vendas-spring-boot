70bd813e3900fbe5bc043c4b844d992ff5b334cf
luispaulos <luispaulo10@gmail.com>
2020-12-24 17:11:26 -0300

    Criando perfis de configuração de desenvolvimento e produção

--- src/main/java/org/spring/lp/MinhaConfiguration.java
+++ src/main/java/org/spring/lp/MinhaConfiguration.java
@@ -1,8 +1,17 @@
 package org.spring.lp;
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("development")
 public class MinhaConfiguration {
+
+    @Bean
+    public CommandLineRunner executar(){
+        return args -> System.out.println("Rodando a Configuração de Desenvolvimento.");
+    }
 
 }
