a357c3606d77f509ed205e7631e6715cf1bf4acc
luispaulos <luispaulo10@gmail.com>
2020-12-24 16:43:35 -0300

    Utilizando arquivo de propriedades para obter nome da aplicação

--- src/main/java/org/spring/lp/VendasController.java
+++ src/main/java/org/spring/lp/VendasController.java
@@ -1,15 +1,13 @@
 package org.spring.lp;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class VendasController {
 
-    @Autowired
-    @Qualifier(value = "applicationName")
+    @Value("${application.name}")
     public String applicationName;
 
     @GetMapping("/hello")
