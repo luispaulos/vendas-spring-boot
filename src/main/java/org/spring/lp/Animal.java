502c59746f2a39f6194178025a3bef508b3ca063
luispaulos <luispaulo10@gmail.com>
2020-12-25 11:30:48 -0300

    Criação de annotations customizadas baseadas no exemplo de interface Animal

---
+++ src/main/java/org/spring/lp/Animal.java
@@ -0,0 +1,6 @@
+package org.spring.lp;
+
+public interface Animal {
+
+    void fazerBarulho();
+}
--- src/main/java/org/spring/lp/VendasController.java
+++ src/main/java/org/spring/lp/VendasController.java
@@ -1,6 +1,8 @@
 package org.spring.lp;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,7 +10,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class VendasController {
 
     @Value("${application.name}")
-    public String applicationName;
+    private String applicationName;
+
+    @Cachorro
+    private Animal animal;
+
+    @Bean(name = "executarAnimal")
+    public CommandLineRunner executar(){
+        return args -> this.animal.fazerBarulho();
+    }
 
     @GetMapping("/hello")
     public String helloWorld(){
