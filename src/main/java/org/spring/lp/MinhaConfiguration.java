cb347600cf59f0201015313f4b310abfa37766ba
luispaulos <luispaulo10@gmail.com>
2020-12-25 11:07:41 -0300

    Criando annotation de perfil Development

--- src/main/java/org/spring/lp/MinhaConfiguration.java
+++ src/main/java/org/spring/lp/MinhaConfiguration.java
@@ -2,11 +2,8 @@ package org.spring.lp;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
-@Configuration
-@Profile("development")
+@Development
 public class MinhaConfiguration {
 
     @Bean
