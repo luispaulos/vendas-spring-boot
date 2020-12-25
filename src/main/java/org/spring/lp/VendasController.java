44043874e25b8b93df0006c1ecf4c76982d6116c
luispaulos <luispaulo10@gmail.com>
2020-12-25 16:50:06 -0300

    Criando primeiras operações com JDBC salvar e obterTodos

--- src/main/java/org/spring/lp/VendasController.java
+++ src/main/java/org/spring/lp/VendasController.java
@@ -1,10 +1,16 @@
 package org.spring.lp;
 
+import org.spring.lp.domain.entity.Cliente;
+import org.spring.lp.domain.entity.domain.repositorio.ClienteJDBCRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class VendasController {
@@ -14,6 +20,17 @@ public class VendasController {
 
     @Cachorro
     private Animal animal;
+
+    @Bean
+    public CommandLineRunner inserirClientes(@Autowired ClienteJDBCRepositorio repositorio){
+        return args ->{
+            repositorio.salvar(new Cliente("Luis Paulo"));
+            repositorio.salvar(new Cliente("Aline"));
+
+            List<Cliente> todos = repositorio.obterTodos();
+            todos.forEach(System.out::println);
+        };
+    }
 
     @Bean(name = "executarAnimal")
     public CommandLineRunner executar(){
@@ -28,5 +45,13 @@ public class VendasController {
     @GetMapping("/sistema")
     public String nomeAplicacao(){
         return applicationName;
+    }
+
+    @GetMapping("/clientes")
+    public List<String> todosClientes(@Autowired ClienteJDBCRepositorio clienteRepositorio){
+        List<Cliente> todos = clienteRepositorio.obterTodos();
+        List<String> nomes = new ArrayList<>();
+        todos.forEach(cliente -> nomes.add(cliente.getNome()));
+        return nomes;
     }
 }
