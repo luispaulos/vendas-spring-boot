9628b0a5fd96471b3806576f47062cc363f2bb30
luispaulos <luispaulo10@gmail.com>
2020-12-26 17:54:51 -0300

    Concluindo CRUD JPA para Cliente

--- src/main/java/org/spring/lp/domain/entity/domain/repositorio/ClienteJPARepositorio.java
+++ src/main/java/org/spring/lp/domain/entity/domain/repositorio/ClienteJPARepositorio.java
@@ -4,7 +4,6 @@ import org.spring.lp.domain.entity.Cliente;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
-
 import javax.persistence.EntityManager;
 import java.util.List;
 
@@ -21,7 +20,32 @@ public class ClienteJPARepositorio {
     }
 
     @Transactional
+    public Cliente atualizar(Cliente cliente){
+        return entityManager.merge(cliente);
+    }
+
+    @Transactional(readOnly = true)
     public List<Cliente> obterTodos(){
         return entityManager.createQuery("from Cliente").getResultList();
+    }
+
+    @Transactional(readOnly = true)
+    public List<Cliente> buscarPorNome(String nome){
+        return  entityManager.createQuery("select c from Cliente c where c.nome like :nome", Cliente.class)
+                .setParameter("nome", "%" + nome + "%").getResultList();
+    }
+
+    @Transactional
+    public void deletar(Cliente cliente){
+        if(!entityManager.contains(cliente)){
+            cliente = entityManager.merge(cliente);
+        }
+        entityManager.remove(cliente);
+    }
+
+    @Transactional
+    public void deletar(Integer id){
+        Cliente cliente = entityManager.find(Cliente.class, id);
+        deletar(cliente);
     }
 }
