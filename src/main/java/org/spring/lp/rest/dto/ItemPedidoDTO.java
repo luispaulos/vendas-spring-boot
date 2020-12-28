a0cd8b15af63220a50af2c526d3bf741bb8ab33c
luispaulos <luispaulo10@gmail.com>
2020-12-28 16:39:53 -0300

    Criando DTOs PedidoDTO e ItemPedidoDTO

---
+++ src/main/java/org/spring/lp/rest/dto/ItemPedidoDTO.java
@@ -0,0 +1,7 @@
+package org.spring.lp.rest.dto;
+
+public class ItemPedidoDTO {
+
+    private Integer produto;
+    private Integer quantidade;
+}
---
+++ src/main/java/org/spring/lp/rest/dto/PedidoDTO.java
@@ -0,0 +1,12 @@
+package org.spring.lp.rest.dto;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public class PedidoDTO {
+
+    private Integer cliente;
+    private BigDecimal total;
+    private List<ItemPedidoDTO> items;
+
+}
