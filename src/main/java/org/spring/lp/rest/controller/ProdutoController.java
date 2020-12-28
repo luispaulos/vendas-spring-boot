f460db3840de773f3edea85ae69f928f60d2d043
luispaulos <luispaulo10@gmail.com>
2020-12-28 15:43:23 -0300

    Implementando API de Produtos

---
+++ src/main/java/org/spring/lp/rest/controller/ProdutoController.java
@@ -0,0 +1,57 @@
+package org.spring.lp.rest.controller;
+
+import org.spring.lp.domain.entity.Produto;
+import org.spring.lp.domain.repositorio.IProdutoJPARepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/produtos")
+public class ProdutoController {
+
+    @Autowired
+    private IProdutoJPARepositorio produtoJPARepositorio;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Produto salvar(@RequestBody Produto produto){
+        return produtoJPARepositorio.save(produto);
+    }
+
+    @PutMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void atualizar(@PathVariable Integer id, @RequestBody Produto produto){
+        produtoJPARepositorio.findById(id).map(p -> {
+            produto.setId(p.getId());
+            produtoJPARepositorio.save(produto);
+            return p;
+        }).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Produto não encontrado."));
+    }
+
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletar(@PathVariable Integer id){
+        produtoJPARepositorio.findById(id).map(p -> {
+            produtoJPARepositorio.delete(p);
+            return p;
+        }).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Produto não encontrado."));
+    }
+
+    @GetMapping
+    public List<Produto> find(Produto filtro){
+        ExampleMatcher matcher = ExampleMatcher.matching().withIgnoreCase().withStringMatcher(ExampleMatcher.StringMatcher.CONTAINING);
+        Example<Produto> example = Example.of(filtro, matcher);
+        return produtoJPARepositorio.findAll(example);
+    }
+
+    @GetMapping("{id}")
+    public Produto getProdutoById(@PathVariable Integer id){
+        return produtoJPARepositorio.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,"Produto não encontrado."));
+    }
+
+}
