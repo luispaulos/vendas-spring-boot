206dbc1a8f4c39de4c85b23957ac0af80f6cc5cd
luispaulos <luispaulo10@gmail.com>
2020-12-29 06:55:52 -0300

    Criando classe de exceção para o projeto RegraNegocioException

---
+++ src/main/java/org/spring/lp/exception/RegraNegocioException.java
@@ -0,0 +1,8 @@
+package org.spring.lp.exception;
+
+public class RegraNegocioException extends RuntimeException{
+
+    public RegraNegocioException(String message) {
+        super(message);
+    }
+}
