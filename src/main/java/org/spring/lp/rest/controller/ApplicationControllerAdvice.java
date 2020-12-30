213fc823f541359b4fff9339c3bbd503ce02ef10
luispaulos <luispaulo10@gmail.com>
2020-12-30 10:58:57 -0300

    Criando primeiras validações para Cliente com Bean Validation

--- src/main/java/org/spring/lp/rest/controller/ApplicationControllerAdvice.java
+++ src/main/java/org/spring/lp/rest/controller/ApplicationControllerAdvice.java
@@ -3,9 +3,13 @@ package org.spring.lp.rest.controller;
 import org.spring.lp.exception.PedidoNaoEncontradoException;
 import org.spring.lp.exception.RegraNegocioException;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class ApplicationControllerAdvice {
@@ -20,5 +24,14 @@ public class ApplicationControllerAdvice {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public String handlePedidoNaoEncontradoException(PedidoNaoEncontradoException ex){
         return ex.getMessage();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public List<String> handleMethodArgNotValidException(MethodArgumentNotValidException ex){
+        return ex.getBindingResult().getAllErrors()
+                .stream()
+                .map(erro -> erro.getDefaultMessage())
+                .collect(Collectors.toList());
     }
 }
