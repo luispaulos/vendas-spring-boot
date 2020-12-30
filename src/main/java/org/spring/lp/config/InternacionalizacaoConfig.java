0b906b7f51649686fe1751adaca07d4278a27636
luispaulos <luispaulo10@gmail.com>
2020-12-30 14:55:03 -0300

    Colocando comentários nas classes e arquivo de propriedade de internacionalização

--- src/main/java/org/spring/lp/config/InternacionalizacaoConfig.java
+++ src/main/java/org/spring/lp/config/InternacionalizacaoConfig.java
@@ -8,11 +8,20 @@ import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 import java.util.Locale;
 
+/**
+ * Classe responsável pelo carregamento e configuração do arquivo de mensagens 'messages.properties'
+ * utilizado para internacionalização de mensagens.
+ */
 @Configuration
 public class InternacionalizacaoConfig {
 
+    /**
+     * Bean responsável por carregar o arquivo messages.propergies
+     *
+     * @return
+     */
     @Bean
-    public MessageSource messageSource(){
+    public MessageSource messageSource() {
         ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
         messageSource.setBasename("classpath:messages");
         messageSource.setDefaultEncoding("ISO-8859-1");
@@ -20,8 +29,13 @@ public class InternacionalizacaoConfig {
         return messageSource;
     }
 
+    /**
+     * Bean responsável por obter um ResourceBundle e colocá-lo disponível no contexto do spring
+     *
+     * @return
+     */
     @Bean
-    public LocalValidatorFactoryBean validatorFactoryBean(){
+    public LocalValidatorFactoryBean validatorFactoryBean() {
         LocalValidatorFactoryBean validatorFactoryBean = new LocalValidatorFactoryBean();
         validatorFactoryBean.setValidationMessageSource(messageSource());
         return validatorFactoryBean;
